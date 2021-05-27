495e3161cb234e2bfe2cc6efb82db530f1843709
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2021-05-27 14:32:13 +0300

    react router dom eklendi active class yapıldı

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Player from './components/Player/Player';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Sidebar />
+      <Switch>
+        <Route path="/" />
+      </Switch>
       <Player />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Sidebar.scss';
 
 function Sidebar() {
-  const [currentLocation, setcurrentLocation] = useState(window.location.href);
+  const location = useLocation();
   useEffect(() => {
     const currentLocation = window.location.href;
     const sideBarItems = document.querySelectorAll('.sidebar__item');
@@ -13,56 +14,56 @@ function Sidebar() {
         item.className = 'sidebar__item';
       }
     });
-  }, [currentLocation]);
+  }, [location]);
 
   return (
     <div className="sidebar">
       <div className="sidebar__brand">MUSICLY</div>
       <div className="sidebar--top">
-        <a href="#home" className="sidebar__item">
+        <Link to="/" className="sidebar__item">
           <i className="fas fa-home sidebar__icon  " />
           <h2 className="sidebar__text">HOME</h2>
-        </a>
-        <a href="#browse" className="sidebar__item">
+        </Link>
+        <Link to="/browse" className="sidebar__item">
           <i className="fas fa-search sidebar__icon" />
           <h2 className="sidebar__text">BROWSE</h2>
-        </a>
-        <a href="#radıo" className="sidebar__item">
+        </Link>
+        <Link to="/radıo" className="sidebar__item">
           <i className="fas fa-microphone sidebar__icon" />
           <h2 className="sidebar__text">RADIO</h2>
-        </a>
+        </Link>
       </div>
       <div className="sidebar--centered">
         <div className="sidebar__text sidebar__text-secondary">YOUR LIBRARY</div>
-        <a href="#recently-played" className="sidebar__item">
+        <Link to="/recently-played" className="sidebar__item">
           <i className="far fa-clock sidebar__icon" />
           <h2 className="sidebar__text">RECENTLY PLAYED</h2>
-        </a>
-        <a href="#favorıte-songs" className="sidebar__item">
+        </Link>
+        <Link to="/favorıte-songs" className="sidebar__item">
           <i className="far fa-heart  sidebar__icon" />
           <h2 className="sidebar__text">FAVORITE SONGS</h2>
-        </a>
-        <a href="#artıst" className="sidebar__item">
+        </Link>
+        <Link to="/artıst" className="sidebar__item">
           <i className="far fa-user sidebar__icon" />
           <h2 className="sidebar__text">ARTIST</h2>
-        </a>
-        <a href="#albums" className="sidebar__item">
-          <img src="./images/albums.svg" className=" sidebar__icon" />
+        </Link>
+        <Link to="/albums" className="sidebar__item">
+          <i className="fas fa-compact-disc  sidebar__icon" />
           <h2 className="sidebar__text">ALBUMS</h2>
-        </a>
+        </Link>
       </div>
       <div className="sidebar--bottom">
         <div className="sidebar__text sidebar__text-secondary">PLAYLIST</div>
-        <a href="#playlist" className="sidebar__item">
+        <Link to="/playlist" className="sidebar__item">
           <h2 className="sidebar__text">HINDI TOP 50 </h2>
-        </a>
-        <a href="#playlist" className="sidebar__item">
+        </Link>
+        <Link to="/playlist" className="sidebar__item">
           <h2 className="sidebar__text">INDIPOP SONGS</h2>
-        </a>
-        <a href="#playlist" className="sidebar__item">
+        </Link>
+        <Link to="/playlist" className="sidebar__item">
           <h2 className="sidebar__text">ROMANCE SEASON</h2>
-        </a>
-      </div>{' '}
+        </Link>
+      </div>
       <div className="sidebar__button">
         <i className="fas fa-plus-circle sidebar__button__icon sidebar__icon-secondary" />
         <h2 className="sidebar__text sidebar__text-green">ADD NEW PLAYLIST</h2>
