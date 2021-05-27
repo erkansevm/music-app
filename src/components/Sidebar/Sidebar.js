3ec7e9c2bd07cc9070b07084da76ceaff629519c
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2021-05-27 11:44:51 +0300

    sidebar  with active class-1

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,50 +6,50 @@ function Sidebar() {
     <div className="sidebar">
       <div className="sidebar__brand">MUSICLY</div>
       <div className="sidebar--top">
-        <div className="sidebar__item">
-          <i className="fas fa-home sidebar__icon" />
+        <a href="#home" className="sidebar__item sidebar__item-active">
+          <i className="fas fa-home sidebar__icon  " />
           <h2 className="sidebar__text">HOME</h2>
-        </div>
-        <div className="sidebar__item">
+        </a>
+        <a href="#" className="sidebar__item">
           <i className="fas fa-search sidebar__icon" />
           <h2 className="sidebar__text">BROWSE</h2>
-        </div>
-        <div className="sidebar__item">
+        </a>
+        <a href="#" className="sidebar__item">
           <i className="fas fa-microphone sidebar__icon" />
           <h2 className="sidebar__text">RADIO</h2>
-        </div>
+        </a>
       </div>
       <div className="sidebar--centered">
         <div className="sidebar__text sidebar__text-secondary">YOUR LIBRARY</div>
-        <div className="sidebar__item">
+        <a href="#" className="sidebar__item">
           <i className="far fa-clock sidebar__icon" />
           <h2 className="sidebar__text">RECENTLY PLAYED</h2>
-        </div>
-        <div className="sidebar__item">
+        </a>
+        <a href="#" className="sidebar__item">
           <i className="far fa-heart  sidebar__icon" />
           <h2 className="sidebar__text">FAVORITE SONGS</h2>
-        </div>
-        <div className="sidebar__item">
+        </a>
+        <a href="#" className="sidebar__item">
           <i className="far fa-user sidebar__icon" />
           <h2 className="sidebar__text">ARTIST</h2>
-        </div>
-        <div className="sidebar__item">
+        </a>
+        <a href="#" className="sidebar__item">
           <img src="./images/albums.svg" className=" sidebar__icon" />
           <h2 className="sidebar__text">ALBUMS</h2>
-        </div>
+        </a>
       </div>
       <div className="sidebar--bottom">
         <div className="sidebar__text sidebar__text-secondary">PLAYLIST</div>
-        <div className="sidebar__item">
+        <a href="#" className="sidebar__item">
           <h2 className="sidebar__text">HINDI TOP 50 </h2>
-        </div>
-        <div className="sidebar__item">
+        </a>
+        <a href="#" className="sidebar__item">
           <h2 className="sidebar__text">INDIPOP SONGS</h2>
-        </div>
-        <div className="sidebar__item">
+        </a>
+        <a href="#" className="sidebar__item">
           <h2 className="sidebar__text">ROMANCE SEASON</h2>
-        </div>
-      </div>
+        </a>
+      </div>{' '}
       <div className="sidebar__button">
         <i className="fas fa-plus-circle sidebar__button__icon sidebar__icon-secondary" />
         <h2 className="sidebar__text sidebar__text-green">ADD NEW PLAYLIST</h2>
