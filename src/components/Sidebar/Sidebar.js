3ff77c97c1c78d695c0d5c53136cef0e5a2a415a
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2021-05-27 14:08:33 +0300

    before react-router-dom

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,52 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Sidebar.scss';
 
 function Sidebar() {
+  const [currentLocation, setcurrentLocation] = useState(window.location.href);
+  useEffect(() => {
+    const currentLocation = window.location.href;
+    const sideBarItems = document.querySelectorAll('.sidebar__item');
+    sideBarItems.forEach((item) => {
+      if (item.href === currentLocation) {
+        item.className = 'sidebar__item sidebar__item-active';
+      } else {
+        item.className = 'sidebar__item';
+      }
+    });
+  }, [currentLocation]);
+
   return (
     <div className="sidebar">
       <div className="sidebar__brand">MUSICLY</div>
       <div className="sidebar--top">
-        <a href="#home" className="sidebar__item sidebar__item-active">
+        <a href="#home" className="sidebar__item">
           <i className="fas fa-home sidebar__icon  " />
           <h2 className="sidebar__text">HOME</h2>
         </a>
-        <a href="#" className="sidebar__item">
+        <a href="#browse" className="sidebar__item">
           <i className="fas fa-search sidebar__icon" />
           <h2 className="sidebar__text">BROWSE</h2>
         </a>
-        <a href="#" className="sidebar__item">
+        <a href="#radıo" className="sidebar__item">
           <i className="fas fa-microphone sidebar__icon" />
           <h2 className="sidebar__text">RADIO</h2>
         </a>
       </div>
       <div className="sidebar--centered">
         <div className="sidebar__text sidebar__text-secondary">YOUR LIBRARY</div>
-        <a href="#" className="sidebar__item">
+        <a href="#recently-played" className="sidebar__item">
           <i className="far fa-clock sidebar__icon" />
           <h2 className="sidebar__text">RECENTLY PLAYED</h2>
         </a>
-        <a href="#" className="sidebar__item">
+        <a href="#favorıte-songs" className="sidebar__item">
           <i className="far fa-heart  sidebar__icon" />
           <h2 className="sidebar__text">FAVORITE SONGS</h2>
         </a>
-        <a href="#" className="sidebar__item">
+        <a href="#artıst" className="sidebar__item">
           <i className="far fa-user sidebar__icon" />
           <h2 className="sidebar__text">ARTIST</h2>
         </a>
-        <a href="#" className="sidebar__item">
+        <a href="#albums" className="sidebar__item">
           <img src="./images/albums.svg" className=" sidebar__icon" />
           <h2 className="sidebar__text">ALBUMS</h2>
         </a>
       </div>
       <div className="sidebar--bottom">
         <div className="sidebar__text sidebar__text-secondary">PLAYLIST</div>
-        <a href="#" className="sidebar__item">
+        <a href="#playlist" className="sidebar__item">
           <h2 className="sidebar__text">HINDI TOP 50 </h2>
         </a>
-        <a href="#" className="sidebar__item">
+        <a href="#playlist" className="sidebar__item">
           <h2 className="sidebar__text">INDIPOP SONGS</h2>
         </a>
-        <a href="#" className="sidebar__item">
+        <a href="#playlist" className="sidebar__item">
           <h2 className="sidebar__text">ROMANCE SEASON</h2>
         </a>
       </div>{' '}
