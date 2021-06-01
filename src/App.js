038252ab4f102dd660a776c4b59f6b58e1147b59
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2021-06-01 11:15:12 +0300

    navbar kaba

--- src/App.js
+++ src/App.js
@@ -4,27 +4,26 @@ import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Featured from './components/Features/Featured';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <Router>
-    <div className="container">
-      <div className="container-top">
-        <Sidebar />
-        <Switch>
-          <Route path="/" />
-        </Switch>
-        <div className="content">
-          <Featured />
+      <div className="container">
+        <div className="container-top">
+          <Sidebar />
+          <Switch>
+            <Route path="/" />
+          </Switch>
+          <div className="content">
+            <Navbar />
+            <Featured />
+          </div>
         </div>
-        
+        <div className="container-bot">
+          <Player />
+        </div>
       </div>
-      <div className="container-bot">
-        <Player />
-      </div>
-      
-    </div>
-     
     </Router>
   );
 }
